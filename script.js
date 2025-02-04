f032d530d40236bbde99ae12a678981ba88ef445
Nim <nimshrestha3@gmail.com>
2025-02-04 14:39:32 +0545

    Create operate function

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function add(num1,num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1,num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1,num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1,num2) {
+function divide(num1, num2) {
     return num1 / num2;
+}
+let operand1;
+let operator;
+let operand2;
+
+function operate(operand1, operator, operand2) {
+    console.log(`${operand1} ${operator} ${operand2}`);
+    switch(operator){
+        case '+':
+            return add(operand1, operand2);
+        
+        case '-':
+            return subtract(operand1, operand2);
+        
+        case '*':
+            return multiply(operand1, operand2);
+        
+        case '/':
+            return divide(operand1, operand2);
+    }
 }
